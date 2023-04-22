fb655089ffcc89b672fa049bf709852aad8f13d0
JEAN2001KIA <131545884+JEAN2001KIA@users.noreply.github.com>
2023-04-22 12:43:31 -0500

    Add files via upload

---
+++ EV_1_ALEJANDRO_URQUIA/ale.py
@@ -0,0 +1,11 @@
+# escribiremos un programa el nombre del usuario en la consola y despues que el usuario lo introdusca sese muestre en la pantalla el saludo de "hola" seguido del nombre del usuario #
+
+nombre = input ("introduce tu nombre: ")
+print ("¡hola " + nombre + "!")
+
+
+
+
+
+    
+     
